7fee6c2ba995b580b5cb7c03ead6e6cd96992971
milohill <116620023+milohill@users.noreply.github.com>
2023-06-22 12:05:04 +0900

    Finish the post template

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-// fetch posts here
+import React, { useState, useEffect } from 'react';
+import Post from './Post';
 
 const Blog = () => {
-  return <div className="board-content blog">Blog</div>;
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const [posts, setPosts] = useState([]);
+
+  async function fetchPosts() {
+    const response = await fetch('http://localhost:3000/api/posts');
+    const json = await response.json();
+    setPosts(json);
+  }
+
+  return (
+    <div className="board-content blog">
+      {posts.map((el) => (
+        <Post obj={el} />
+      ))}
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 const Post = (props) => {
+  // have the function run on the initial rendering
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
   // destructure the props object
-  const { _id, title, content, author, date } = props.el;
+  const { _id, title, content, author, date } = props.obj;
 
   // format the date object
   const options = {
@@ -14,21 +19,25 @@ const Post = (props) => {
   };
   const formattedDate = new Date(date).toLocaleDateString('en-US', options);
 
-  // have the function run on the initial rendering
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
   const [comments, setComments] = useState({});
 
-  const fetchComments = async () => {
+  async function fetchComments() {
     const response = await fetch(`http://localhost:3000/api/comments/${_id}`);
     const json = await response.json();
     setComments(json);
-  };
+  }
 
   return (
     <div className="post">
+      <div>
+        <h2>{title}</h2>
+        <div>
+          <p>AUTHOR</p>
+          <div>{formattedDate}</div>
+        </div>
+      </div>
+      <div>{content}</div>
+      <div>Comments ({comments.length})</div>
     </div>
   );
 };
