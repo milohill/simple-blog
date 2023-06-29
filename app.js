74610e7d901f8a588d299adacb9d29c10aade43b
milohill <116620023+milohill@users.noreply.github.com>
2023-06-30 06:53:10 +0900

    Modify admin model's name field to make it distinguishable from the default id field in mongo db

---
+++ app.js
@@ -0,0 +1,79 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const Admin = require('./models/Admin');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.MONGO_DB_URL;
+
+const main = async () => {
+  await mongoose.connect(mongoDB);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
+
+// passport configuration
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'name',
+    passwordField: 'password'
+  },
+  function(name, password, cb) {
+      Admin.findOne({ id: username })
+          .then((user) => {
+              if (!user) { return cb(null, false) }
+              
+              // Function defined at bottom of app.js
+              const isValid = validPassword(password, user.hash, user.salt);
+              
+              if (isValid) {
+                  return cb(null, user);
+              } else {
+                  return cb(null, false);
+              }
+          })
+          .catch((err) => {   
+              cb(err);
+          });
+}));
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
