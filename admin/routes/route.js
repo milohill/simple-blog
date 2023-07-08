8ff9523d4a28ff977259591297daa3ba4c0a0229
milohill <116620023+milohill@users.noreply.github.com>
2023-07-09 00:47:08 +0900

    Finish the front page showing the list of posts from the server

--- admin/routes/route.js
+++ admin/routes/route.js
@@ -3,11 +3,24 @@ var router = express.Router();
 const passport = require('passport');
 const { body, validationResult } = require('express-validator');
 
-router.get('/', function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/login');
-  }
-  res.render('index')
+router.get('/', async function(req, res, next) {
+  // if (!req.isAuthenticated()) {
+  //   return res.redirect('/login');
+  // }
+  const response = await fetch('http://localhost:3000/api/posts');
+  const posts = await response.json();
+  console.log(posts);
+
+  const updatedPosts = await Promise.all(posts.map(async function(post) {
+    const commentResponse = await fetch(`http://localhost:3000/api/comments/${post._id}`);
+    const comments = await commentResponse.json();
+    post.comments = comments;
+    return post;
+  }));
+
+  res.render('index', {
+    posts: updatedPosts
+  });
 });
 
 router.get('/login', function(req, res, next) {
@@ -42,7 +55,7 @@ router.post('/create', async (req, res) => {
   if (!req.isAuthenticated()) {
     return res.redirect('/login');
   }
-  
+
   const { title, content } = req.body;
   const published = req.body.published ? true : false;
   const author = res.locals.user._id;
